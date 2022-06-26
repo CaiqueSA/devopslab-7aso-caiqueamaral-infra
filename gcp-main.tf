57f66ba1ca38e0159e5db3433408847bb76a496c
Caique Santos Amaral <caiqu.7@gmail.com>
2022-06-26 12:06:26 -0300

    Ajustando registry e app

--- gcp-main.tf
+++ gcp-main.tf
@@ -7,7 +7,7 @@ resource "google_artifact_registry_repository" "my-repo" {
   provider = google-beta
 
   location = "us-central1"
-  repository_id = "labdevopscloud7"
+  repository_id = "labdevops"
   description = "Image DOCKER"
   format = "DOCKER"
 }
